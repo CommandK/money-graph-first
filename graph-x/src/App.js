d038d02ab1003367faf185ec31205b6b79b521a9
Khuthadzo Masindi <khuthadzo.masindi@gmail.com>
2023-03-18 14:35:47 +0000

    MUI installed

--- graph-x/src/App.js
+++ graph-x/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Typography } from '@mui/material';
+//import { styled } from '@mui/system';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Typography variant="h4" component="h1">
+        My React 18 Material-UI App
+      </Typography>
+
+
   );
 }
 
